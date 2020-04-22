6473840dc772c1941c417c9ee778d2c85795d05b
Shlomi Kushchi <shlomikushchi@users.noreply.github.com>
2020-04-22 21:00:32 +0300

    The Polygon Quote mapping did not change.
    we need to use the same mapping as before

--- alpaca_trade_api/polygon/entity.py
+++ alpaca_trade_api/polygon/entity.py
@@ -329,16 +329,16 @@ trade_mapping = {
     "t": "timestamp"
 }
 
-quote_mapping = {
-    "T": "symbol",
-    "X": "askexchange",
-    "P": "askprice",
-    "S": "asksize",
-    "x": "bidexchange",
-    "p": "bidprice",
-    "s": "bidsize",
-    "c": "conditions",
-    "t": "timestamp"
+quote_mapping = {	
+    "sym": "symbol",	
+    "ax": "askexchange",	
+    "ap": "askprice",	
+    "as": "asksize",	
+    "bx": "bidexchange",	
+    "bp": "bidprice",	
+    "bs": "bidsize",	
+    "c": "condition",	
+    "t": "timestamp"	
 }
 
 agg_mapping = {
